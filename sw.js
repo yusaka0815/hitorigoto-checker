0504b9639de9dd75a00b2de4395ec192f17a96c5
yusaka0815 <16935503+yusaka0815@users.noreply.github.com>
2026-03-25 13:03:03 +0900

    feat: PWA対応・全画面・スクロールなしレイアウト v1.5.0

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'hitorigoto-v1';
+const FILES = ['./', './index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
